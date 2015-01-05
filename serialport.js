37925b8aafb5a9c19b6dc2b7a0443ba70e5d1b3b
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-01-05 10:26:55 +0100

    load debug module

--- serialport.js
+++ serialport.js
@@ -9,9 +9,7 @@
 
 var binary = require('node-pre-gyp');
 var path = require('path');
-var PACKAGE_JSON = path.join(__dirname, 'package.json');
-var binding_path = binary.find(path.resolve(PACKAGE_JSON));
-var SerialPortBinding = require(binding_path);
+var SerialPortBinding = require('./build/Debug/serialport.node');
 
 var parsers = require('./parsers');
 var EventEmitter = require('events').EventEmitter;
